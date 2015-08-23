c7cecf572fd7efab99f19a4fac04908e93e1d9ea
Jamie Diprose <jamie.diprose@gmail.com>
2015-08-23 23:06:32 +1200

    Tutorial code

---
+++ js/tutorial.js
@@ -0,0 +1,18 @@
+
+var client = new UoACalendarClient({ apiToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmlnX2lhdCI6MTQyMjQ5ODk0OSwiZXhwIjoxNDIyNDk5MjQ5LCJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6ImRldmVsb3BlciIsImVtYWlsIjoidGVzdEBhdWNrbGFuZC5hYy5ueiJ9.7jLkEBovT2HvT2noL4xdIhddaY8wpZpEVYEDHnnNm1Y"});
+var calendarID;
+
+function createCalendarClick()
+{
+	alert('createCalendarClick button call');
+}
+
+function addEventClick()
+{
+	alert('addEventClick button call');
+}
+
+function findEventClick()
+{
+	alert('findEventClick button call');
+}
